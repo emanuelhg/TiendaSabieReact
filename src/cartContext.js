6e8e0a62a231177425d91ae00c3a0ef2b36acb50
Emanuel guzzetti <ehguzzetti@gmail.com>
2021-11-08 23:58:34 -0300

    Update cartContext.js

--- src/cartContext.js
+++ src/cartContext.js
@@ -7,20 +7,40 @@ export const { Provider } = contexto
 export const CustomProvider = ({ children }) => {
 
     const [cart, setCart] = useState([])
-  
-
-    const addToCart = (detalles) => {
-        setCart([...cart, detalles])
-        console.log(cart)
-            }
 
 
-    const removeFromCart = (product) => {
-        setCart(cart.filter(item => item.id !== product.id))
+    const addToCart = detalles => {
+        setCart(cart.concat(detalles))
+        const temp = (cart.concat(detalles))
+        const newCart = agruparSumar(temp, ['id','title','description','category','price','pictureURL'],['quantity'])
+        setCart(newCart)
+    }
+
+    const agruparSumar = (arr, groupKeys, sumKeys) =>{
+        return Object.values(
+          arr.reduce((acc, curr) => {
+            const group = groupKeys.map(k => curr[k]).join('-');
+            acc[group] = acc[group] || Object.fromEntries(groupKeys.map(k => [k, curr[k]]).concat(sumKeys.map(k => [k, 0])));
+            sumKeys.forEach(k => acc[group][k] += curr[k]);
+            return acc;
+          }, {})
+        );
+      }
+
+    const removeFromCart = id => {
+        setCart(cart.filter(item => item.id !== id))
     }
 
     const clearFromCart = () => {
         setCart([])
+    }
+
+    const sumQuantity = () => {
+        let sum = 0
+        cart.forEach(item => {
+            sum += item.quantity
+        })
+        return sum
     }
 
     const valor_del_contexto = {
@@ -28,7 +48,8 @@ export const CustomProvider = ({ children }) => {
         cart : cart,
         addToCart : addToCart,
         removeFromCart : removeFromCart,
-        clearFromCart : clearFromCart
+        clearFromCart : clearFromCart,
+        sumQuantity : sumQuantity
 
     }
 
